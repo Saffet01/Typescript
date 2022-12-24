bb2cf89b32f7b684db63e855b37c3cc8048ef80b
Saffet01 <saffetr.kaban@gmail.com>
2022-12-25 01:11:29 +0300

    other types and flow control statements

--- 1_Kurulum/app.js
+++ 1_Kurulum/app.js
@@ -44,3 +44,50 @@ console.log(media.newsletter);
 //Union
 var code = "123";
 console.log(code);
+//Any
+var someThing = "Selam";
+console.log(someThing);
+someThing = 321;
+console.log(someThing);
+//void
+function sayHello() {
+    console.log("Hi!");
+}
+sayHello();
+//If else 
+var x = 15;
+var y = 25;
+if (x > y) {
+    console.log("x y'den büyüktür.");
+}
+else {
+    console.log("x y'den küçüktür veya eşittir.");
+}
+// Ternary örneği
+x < y ? console.log("x y'den küçüktür.") : console.log("x y'den büyüktür.");
+//Switch Case
+var day = 2;
+switch (day) {
+    case 0:
+        console.log("Pazar");
+        break;
+    case 1:
+        console.log("pazartesi");
+        break;
+    case 2:
+        console.log("salı");
+        break;
+    default:
+        console.log("böyle bir gün yok");
+        break;
+}
+//For loop
+for (var i = 0; i < 5; i++) {
+    console.log("i değerim", i);
+}
+//While Loop
+var counter = 0;
+while (counter < 5) {
+    console.log(counter);
+    counter++;
+}
--- 1_Kurulum/app.ts
+++ 1_Kurulum/app.ts
@@ -50,7 +50,6 @@ console.log(ogretmen);
 console.log(ogretmen.yas);
 
 
-
 //enum 
 enum media {
     newspaper,
@@ -66,3 +65,68 @@ console.log(media.newsletter);
 let code:string | number = "123";
 console.log(code);
 
+//Any
+let someThing:any = "Selam";
+console.log(someThing);
+
+someThing=321;
+console.log(someThing);
+
+
+//void
+function sayHello(): void{
+    console.log("Hi!")
+}
+
+sayHello();
+
+
+//If else 
+let x:number = 15;
+let y:number = 25;
+
+if(x>y){
+    console.log("x y'den büyüktür.");
+}
+else{
+    console.log("x y'den küçüktür veya eşittir.");
+}
+
+// Ternary örneği
+x<y ? console.log("x y'den küçüktür.") : console.log("x y'den büyüktür.");
+
+
+//Switch Case
+let day:number = 2;
+
+switch(day){
+    case 0:
+        console.log("Pazar");
+        break;
+
+    case 1: 
+        console.log("pazartesi");
+        break;
+
+    case 2:
+        console.log("salı");
+        break;
+
+    default:
+        console.log("böyle bir gün yok");
+        break;
+}
+
+
+//For loop
+for(let i = 0; i<5; i++){
+    console.log("i değerim",i);
+}
+
+
+//While Loop
+let counter =0;
+while(counter<5){
+    console.log(counter);
+    counter++;
+}
